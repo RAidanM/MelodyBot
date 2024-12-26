e06efa30813de4a00faec8231553f68bb0fa97e1
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-12-25 23:11:18 -0500

    Changed print statements to standard logging module.

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-import os
+import logging
+from dotenv import dotenv_values
 import discord
 import discord.ext
 import discord.ext.commands
-from dotenv import dotenv_values
 
+
+logger = logging.getLogger('discord')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = discord.ext.commands.Bot(command_prefix='!', intents=intents)
 
+
 @bot.event
 async def on_ready():
-    print(f'Ready: {bot.user}')
+    logger.info(f'Ready: {bot.user}')
+
 
 @bot.command()
 async def hello(ctx: discord.ext.commands.Context, arg):
-    print('hi')
-    print(arg)
+    logger.info(f'Received hello command with arg {arg}')
     await ctx.send('Hi lol')
 
 
 if __name__ == '__main__':
     config = dotenv_values(".env")
+    if 'BOT_TOKEN' not in config or not config['BOT_TOKEN']:
+        raise Exception('BOT_TOKEN must be defined in .env file')
     bot.run(config['BOT_TOKEN'])
