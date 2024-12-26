8e9a958a77569bf59cefb9943a1dc8e98559fc5f
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-12-25 23:50:22 -0500

    Bot now plays sound effect.

--- main.py
+++ main.py
@@ -19,9 +19,29 @@ async def on_ready():
 
 
 @bot.command()
-async def hello(ctx: discord.ext.commands.Context, arg):
-    logger.info(f'Received hello command with arg {arg}')
-    await ctx.send('Hi lol')
+async def ping(ctx: discord.ext.commands.Context):
+    logger.info(f'Received ping command from {ctx.author}')
+    await ctx.send('Pong!')
+
+@bot.command()
+async def play(ctx: discord.ext.commands.Context, url: str):
+    logger.info(f'Received play command from {ctx.author} for URL {url}')
+
+    voice_state = ctx.author.voice
+    voice_channel = voice_state.channel if voice_state is not None else None
+    if not voice_channel:
+        await ctx.send(
+            'The `play` command must be sent while in a voice channel!'
+        )
+        return
+
+    voice_client = await voice_channel.connect()
+
+    # sound_effect.mp3 is a royalty-free sound found at
+    # https://pixabay.com/sound-effects/notification-22-270130/
+    voice_client.play(discord.FFmpegPCMAudio('./sound_effect.mp3'))
+
+    await ctx.send('Playing')
 
 
 if __name__ == '__main__':
